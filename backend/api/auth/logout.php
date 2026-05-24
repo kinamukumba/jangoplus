6f6e3c0115465eaf5dac595fad36a4d0dbcf69fd
kinamukumba <kinamukumba@gmail.com>
2026-05-24 21:15:09 +0100

    conf front

--- backend/api/auth/logout.php
+++ backend/api/auth/logout.php
@@ -1,5 +1,5 @@
 <?php
-header('Access-Control-Allow-Origin: http://localhost');
+header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Credentials: true');
 
 session_start();
--- backend/config/database.php
+++ backend/config/database.php
@@ -3,9 +3,9 @@ require_once __DIR__ . '/env.php';
 
 // Configurações do Banco de Dados via .env
 $host = $_ENV['DB_HOST'] ?? '127.0.0.1';
-$db   = $_ENV['DB_NAME'] ?? 'jangoplus_sekulo';
-$user = $_ENV['DB_USER'] ?? 'root';
-$pass = $_ENV['DB_PASS'] ?? '';
+$db   = $_ENV['DB_NAME'] ?? 'plucian1_jango';
+$user = $_ENV['DB_USER'] ?? 'plucian1_jango_user';
+$pass = $_ENV['DB_PASS'] ?? 'i}dUYu0!B-7=d$p%';
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
